c1b359107d7a554d33148f53b9c059ae6746c002
Beltrán José Aceves Gil <beltran.aceves@udc.es>
2021-12-11 20:38:13 +0100

    JSON complete with years

--- prototyping/wiki_parser.js
+++ prototyping/wiki_parser.js
@@ -2,5 +2,17 @@ const fs = require('fs');
 
 let rawdata = fs.readFileSync('db.json');
 let entries = JSON.parse(rawdata);
+var noYearReferences = [];
 
-console.log(entries);
+for (const entry in entries) {
+    var references = entries[entry].references;
+    for (const referenceId in references) {
+        var reference = references[referenceId];
+        if (reference.year == null) {
+            console.log(reference.id);
+            noYearReferences.push(reference);
+        }
+    }
+}
+
+console.log(noYearReferences.length);
